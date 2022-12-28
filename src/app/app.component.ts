bf5c7639eebe7a11dcb9b590172e55997fd0f4f9
Waleed Q <iamwaq@icloud.com>
2022-12-28 08:29:54 +0300

    Added ngFor,ngIf, click event behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title : string = "Dynamic E-Store";
   currentDate : string = new Date().toLocaleTimeString();
+
+  
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+
+  flag : boolean = false;
+
+
   profile: any;
 
   constructor(){
@@ -16,9 +20,19 @@ export class ProfileComponent {
       location : "Kuwait",
       contact : [
         '9999-989-632',
-        'haneen@kfh.com.kw'
+        '8888-700-989',
+        'haneen@kfh.com.kw',
+        'haneen@gmail.com'
       ]
     };
+  }
+
+  // handleButtonClick(){
+  //   console.log("Button was just clicked..."); 
+  // }
+
+  toggleContactDetails(){
+    this.flag = !this.flag;
   }
 
 }
